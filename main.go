58e7cb156a7b0e661a8b4526a7bdaed99587e321
Denis Fortin <denis.fortin@adevinta.com>
2020-09-19 03:18:13 +0200

    Use puzzle struct and handle command line parameters

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 	"time"
@@ -18,6 +19,18 @@ type apiResponse struct {
 	Answer  string        `json:"answer"`
 	Message string        `json:"message"`
 	Desc    []interface{} `json:"desc"`
+}
+
+type puzzle struct {
+	PuzzleString   string
+	PuzzleSolution string
+	Difficulty     string
+	Cells          [9][9]puzzleCell
+}
+
+type puzzleCell struct {
+	Value         int
+	Possibilities [9]bool
 }
 
 func printSudoku(src string) {
@@ -50,29 +63,32 @@ func printSudoku(src string) {
 	print("-----------------------------------------\n")
 }
 
-func printPuzzleLine(puzzle [9][9]int) {
+func printPuzzleLine(puzzle puzzle) {
 	for lineNumber := 0; lineNumber < 9; lineNumber++ {
 		for columnNumber := 0; columnNumber < 9; columnNumber++ {
-			print(puzzle[lineNumber][columnNumber])
+			print(puzzle.Cells[lineNumber][columnNumber].Value)
 		}
 	}
 	print("\n")
 }
 
-func printPuzzle(puzzle [9][9]int) {
+func printPuzzle(puzzle puzzle) {
 	var lineOutput string
 	var lineNumber int
 
 	print("\033[H\033[2J")
+	print("Difficuty: " + puzzle.Difficulty + "\n")
+	print(puzzle.PuzzleString + "\n")
+	print(puzzle.PuzzleSolution + "\n")
 	for lineNumber = 0; lineNumber < 9; lineNumber++ {
-		if lineNumber > 0 && lineNumber%3 == 0 {
+		if lineNumber%3 == 0 {
 			print("=========================================\n")
 		} else {
 			print("-----------------------------------------\n")
 		}
 		lineOutput = "|"
 		for columnNumber := 0; columnNumber < 9; columnNumber++ {
-			cellValue := strconv.Itoa(puzzle[lineNumber][columnNumber])
+			cellValue := strconv.Itoa(puzzle.Cells[lineNumber][columnNumber].Value)
 			cellContent := " "
 			if cellValue != "0" {
 				cellContent = cellValue
@@ -85,23 +101,23 @@ func printPuzzle(puzzle [9][9]int) {
 		lineOutput += "|\n"
 		print(lineOutput)
 	}
-	print("-----------------------------------------\n")
+	print("=========================================\n")
 	time.Sleep(100 * time.Millisecond)
 }
 
-func printPossibilities(possibilities [9][9][9]bool) {
-	for i := 0; i < 9; i++ {
-		for j := 0; j < 9; j++ {
-			print(fmt.Sprintf("%d, %d: ", i, j))
-			for value := 0; value < 9; value++ {
-				if possibilities[i][j][value] == true {
-					print(fmt.Sprintf("%d", value+1))
-				}
-			}
-			print("\n")
-		}
-	}
-}
+// func printPossibilities(possibilities [9][9][9]bool) {
+// 	for i := 0; i < 9; i++ {
+// 		for j := 0; j < 9; j++ {
+// 			print(fmt.Sprintf("%d, %d: ", i, j))
+// 			for value := 0; value < 9; value++ {
+// 				if possibilities[i][j][value] == true {
+// 					print(fmt.Sprintf("%d", value+1))
+// 				}
+// 			}
+// 			print("\n")
+// 		}
+// 	}
+// }
 
 func checkPossibility(possibilities [9]bool) int {
 	possibility := 0
@@ -116,73 +132,59 @@ func checkPossibility(possibilities [9]bool) int {
 	return possibility
 }
 
-func clearNumber(lineNumber int, columnNumber int, value int, possibilities *[9][9][9]bool) {
+func clearNumber(lineNumber int, columnNumber int, value int, puzzle *puzzle) {
 	for i := 0; i < 9; i++ {
-		possibilities[lineNumber][i][value-1] = false
-		possibilities[i][columnNumber][value-1] = false
-		possibilities[lineNumber][columnNumber][i] = false
+		puzzle.Cells[lineNumber][i].Possibilities[value-1] = false
+		puzzle.Cells[i][columnNumber].Possibilities[value-1] = false
+		puzzle.Cells[lineNumber][columnNumber].Possibilities[i] = false
 	}
 	for i := 0; i < 3; i++ {
 		for j := 0; j < 3; j++ {
-			possibilities[lineNumber/3*3+i][columnNumber/3*3+j][value-1] = false
-			// print(lineNumber/3*3 + i)
+			puzzle.Cells[lineNumber/3*3+i][columnNumber/3*3+j].Possibilities[value-1] = false
 		}
 	}
 }
+func initPuzzle(difficulty string, puzzleLine string, puzzleSolution string) puzzle {
+	var puzzle puzzle
 
-func initialize(src string) ([9][9]int, [9][9][9]bool) {
-	splitSrc := strings.Split(src, "")
-	var puzzle [9][9]int
-	var possibilities [9][9][9]bool
+	puzzle.Difficulty = difficulty
+	puzzle.PuzzleString = puzzleLine
+	puzzle.PuzzleSolution = puzzleSolution
 
+	splitSrc := strings.Split(puzzleLine, "")
 	for lineNumber := 0; lineNumber < 9; lineNumber++ {
 		for columnNumber := 0; columnNumber < 9; columnNumber++ {
 			value, _ := strconv.Atoi(splitSrc[lineNumber*9+columnNumber])
-			puzzle[lineNumber][columnNumber] = value
+			puzzle.Cells[lineNumber][columnNumber].Value = value
 
 			for i := 0; i < 9; i++ {
 				if value == 0 {
-					possibilities[lineNumber][columnNumber][i] = true
+					puzzle.Cells[lineNumber][columnNumber].Possibilities[i] = true
 				} else {
-					possibilities[lineNumber][columnNumber][i] = false
+					puzzle.Cells[lineNumber][columnNumber].Possibilities[i] = false
 				}
 			}
 		}
 	}
-	// printPuzzle(puzzle)
 	for lineNumber := 0; lineNumber < 9; lineNumber++ {
 		for columnNumber := 0; columnNumber < 9; columnNumber++ {
-			value := puzzle[lineNumber][columnNumber]
+			value := puzzle.Cells[lineNumber][columnNumber].Value
 			if value > 0 {
-				clearNumber(lineNumber, columnNumber, value, &possibilities)
+				clearNumber(lineNumber, columnNumber, value, &puzzle)
 			}
 		}
 	}
-	// for lineNumber := 0; lineNumber < 9; lineNumber++ {
-	// 	for columnNumber := 0; columnNumber < 9; columnNumber++ {
-	// 		if puzzle[lineNumber][columnNumber] == 0 {
-	// 			possibility := checkPossibility(possibilities[lineNumber][columnNumber])
-	// 			// print(fmt.Sprintf("%d, %d = %d\n", lineNumber, columnNumber, possibility))
-	// 			if possibility > 0 {
-	// 				print(fmt.Sprintf("%d, %d = %d", lineNumber, columnNumber, possibility))
-	// 				puzzle[lineNumber][columnNumber] = possibility
-	// 			}
-	// 		}
-	// 	}
-	// }
-	// printPuzzle(puzzle)
-	return puzzle, possibilities
+	return puzzle
 }
 
-func checkSquare(line int, column int, puzzle *[9][9]int, possibilities *[9][9][9]bool) {
-	// print(fmt.Sprintf("Check square %d %d\n", line, column))
+func checkSquare(line int, column int, puzzle *puzzle) {
 	for value := 1; value <= 9; value++ {
 		occurences := 0
 		occLine := 0
 		occColumn := 0
 		for i := 0; i < 3; i++ {
 			for j := 0; j < 3; j++ {
-				if possibilities[line+i][column+j][value-1] == true {
+				if puzzle.Cells[line+i][column+j].Possibilities[value-1] == true {
 					occurences++
 					occLine = line + i
 					occColumn = column + j
@@ -190,110 +192,107 @@ func checkSquare(line int, column int, puzzle *[9][9]int, possibilities *[9][9][
 			}
 		}
 		if occurences == 1 {
-			// print(fmt.Sprintf("%d, %d = %d\n", occLine, occColumn, value))
-			puzzle[occLine][occColumn] = value
-			clearNumber(occLine, occColumn, value, possibilities)
+			puzzle.Cells[occLine][occColumn].Value = value
+			clearNumber(occLine, occColumn, value, puzzle)
 			printPuzzle(*puzzle)
 		}
 	}
 }
 
-func checkLine(line int, puzzle *[9][9]int, possibilities *[9][9][9]bool) {
+func checkLine(line int, puzzle *puzzle) {
 	for value := 1; value <= 9; value++ {
 		occurences := 0
 		occColumn := 0
 		for i := 0; i < 9; i++ {
-			if possibilities[line][i][value-1] == true {
+			if puzzle.Cells[line][i].Possibilities[value-1] == true {
 				occurences++
 				occColumn = i
 			}
 		}
 		if occurences == 1 {
-			puzzle[line][occColumn] = value
-			clearNumber(line, occColumn, value, possibilities)
+			puzzle.Cells[line][occColumn].Value = value
+			clearNumber(line, occColumn, value, puzzle)
 			printPuzzle(*puzzle)
 		}
 	}
 }
 
-func checkColumn(column int, puzzle *[9][9]int, possibilities *[9][9][9]bool) {
+func checkColumn(column int, puzzle *puzzle) {
 	for value := 1; value <= 9; value++ {
 		occurences := 0
 		occLine := 0
 		for i := 0; i < 9; i++ {
-			if possibilities[i][column][value-1] == true {
+			if puzzle.Cells[i][column].Possibilities[value-1] == true {
 				occurences++
 				occLine = i
 			}
 		}
 		if occurences == 1 {
-			puzzle[occLine][column] = value
-			clearNumber(occLine, column, value, possibilities)
+			puzzle.Cells[occLine][column].Value = value
+			clearNumber(occLine, column, value, puzzle)
 			printPuzzle(*puzzle)
 		}
 	}
 }
 
-func solve(puzzle *[9][9]int, possibilities *[9][9][9]bool) bool {
+func solve(puzzle *puzzle) bool {
 	for try := 0; try < 10000; try++ {
 		for i := 0; i < 3; i++ {
 			for j := 0; j < 3; j++ {
-				checkSquare(i*3, j*3, puzzle, possibilities)
-				checkLine(i*3+j, puzzle, possibilities)
-				checkColumn(i*3+j, puzzle, possibilities)
+				checkSquare(i*3, j*3, puzzle)
+				checkLine(i*3+j, puzzle)
+				checkColumn(i*3+j, puzzle)
 			}
 		}
 	}
 	return false
 }
 
+// @TODO:
+// - put functions as puzzle methods
+// - add a way to know if puzzle is solved
 func main() {
-	res, err := http.Get("https://sudoku.com/api/getLevel/expert")
-	if err != nil {
-		log.Fatal(err)
+	var difficulty = "easy"
+	var puzzleLine string
+	var puzzleSolution string
+	var puzzle puzzle
+
+	if len(os.Args) == 3 {
+		difficulty = "custom"
+		puzzleLine = os.Args[1]
+		puzzleSolution = os.Args[2]
+	}
+	if len(os.Args) == 2 {
+		difficulty = os.Args[1]
+
+		res, err := http.Get("https://sudoku.com/api/getLevel/" + difficulty)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if res.Body != nil {
+			defer res.Body.Close()
+		}
+
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if res.StatusCode != 200 {
+			log.Fatal(string(body))
+		}
+
+		resJSON := apiResponse{}
+		err = json.Unmarshal(body, &resJSON)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		puzzleLine = fmt.Sprintf("%v", resJSON.Desc[apiPuzzleIdx])
+		puzzleSolution = fmt.Sprintf("%v", resJSON.Desc[apiSolutionIdx])
 	}
 
-	if res.Body != nil {
-		defer res.Body.Close()
-	}
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	resJSON := apiResponse{}
-	err = json.Unmarshal(body, &resJSON)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	puzzleLine := fmt.Sprintf("%v", resJSON.Desc[apiPuzzleIdx])
-	puzzleSolution := fmt.Sprintf("%v", resJSON.Desc[apiSolutionIdx])
-	// print(puzzleLine)
-	// puzzleLine := "000740006406800507700090004030984700820613409040000300062370005005409000070061208"
-
-	puzzle, possibilities := initialize(puzzleLine)
-	printPossibilities(possibilities)
-	printPuzzle(puzzle)
-	solve(&puzzle, &possibilities)
-	printPuzzle(puzzle)
-	printPossibilities(possibilities)
-	print(puzzleLine + "\n")
-	printPuzzleLine(puzzle)
-	print(puzzleSolution + "\n")
-	// for solve(&puzzle, &possibilities) == false {
-	// printPuzzle(puzzle)
-	// }
-
-	// print(possibilities)
-
-	// for i := 0; i < 100; i++ {
-	// 	print("\033[H\033[2J")
-	// 	printSudoku(fmt.Sprintf("%v", resJSON.Desc[apiPuzzleIdx]))
-	// 	print(i)
-	// 	print("\n")
-	// 	time.Sleep(1 * time.Second)
-	// }
-	// fmt.Print(resJSON.Desc[apiSolutionIdx])
+	puzzle = initPuzzle(difficulty, puzzleLine, puzzleSolution)
+	solve(&puzzle)
 }
